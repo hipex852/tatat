510bd1f77087c5e1889aa22f881d281812c80a22
hipex852 <moreira.souza.andre@escola.pr.gov.br>
2025-08-11 22:25:11 -0300

    aa

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
+let player = {
+    x: canvas.width / 2,
+    y: canvas.height - 50,
+    width: 40,
+    height: 40,
+    speed: 5,
+    color: 'blue'
+};
+
+let bullets = [];
+let enemies = [];
+let score = 0;
+let lastEnemyTime = 0;
+const enemySpawnInterval = 1000; // Milissegundos
